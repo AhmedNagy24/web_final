da8ff96e70951e034ae86f097303ee5677e0b393
Ahmed Nagy <anagy464@gmail.com>
2023-05-21 23:52:19 +0300

    Student added

--- web_project/models.py
+++ web_project/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    id = models.CharField(primary_key=True, max_length=8)
+    email = models.EmailField()
+    phone = models.CharField(max_length=11)
+    gender = models.CharField(max_length=10)
+    level = models.IntegerField()
+    department = models.CharField(max_length=10)
+    status = models.CharField(max_length=10)
+    GPA = models.DecimalField(max_digits=3, decimal_places=2)
+    birthdate = models.DateField()
