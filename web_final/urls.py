91c3d877333c6bf83212cd2f049328a178de74d7
Ahmed Nagy <anagy464@gmail.com>
2023-05-21 20:45:27 +0300

    zoz union

--- web_final/urls.py
+++ web_final/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-import web_project
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("web_project.urls"))
 ]
