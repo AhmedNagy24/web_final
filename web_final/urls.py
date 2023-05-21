f419f226b417985b19b3cca0d23304d38457a643
زياد طارق سيد على <95889012+ziyadtarek21@users.noreply.github.com>
2023-05-21 22:46:46 +0300

    added images and finsished login and logout

--- web_final/urls.py
+++ web_final/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("web_project.urls")),
     path('login/', include('web_project.urls')),
-    path('home/', include('web_project.urls')),
+    path('main/', include('web_project.urls')),
 ]
